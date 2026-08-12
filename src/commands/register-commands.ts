eaf3e9755c2a802fe749727cc2ddb214801b9238
MAOZ <maooz4426@gmail.com>
2026-08-13 02:11:22 +0900

    add assert

--- src/commands/register-commands.ts
+++ src/commands/register-commands.ts
@@ -1,12 +1,16 @@
 import { REST, Routes } from "discord.js";
 import { commandList } from "./index.ts";
+import assert from "node:assert";
 // 登録用関数
 async function register() {
-	const rest = new REST().setToken(process.env.TOKEN!);
+    assert.ok(process.env.TOKEN, 'TOKEN is required');
+    assert.ok(process.env.CLIENT_ID, 'CLIENT_ID is required');
 
-	await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), {
-		body: commandList.map((command) => command.data.toJSON()),
-	});
+    const rest = new REST().setToken(process.env.TOKEN);
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+        body: commandList.map((command) => command.data.toJSON()),
+    });
 }
 
 register().catch((err) => console.log(err));
