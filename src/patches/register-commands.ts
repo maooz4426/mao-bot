9196f79bf26f4888ae9a76f23dd45cf94e78e676
MAOZ <maooz4426@gmail.com>
2026-08-14 00:10:28 +0900

    prodでの本番動作確認完了

---
+++ src/patches/register-commands.ts
@@ -0,0 +1,19 @@
+import assert from "node:assert";
+import { REST, Routes } from "discord.js";
+import { commandList } from "../commands";
+
+// 登録用関数
+async function registerCommands() {
+    assert.ok(process.env.TOKEN, "TOKEN is required");
+    assert.ok(process.env.CLIENT_ID, "CLIENT_ID is required");
+
+    const rest = new REST().setToken(process.env.TOKEN);
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+        body: commandList.map((command) => command.data.toJSON()),
+    });
+
+    console.log("finished");
+}
+
+registerCommands().catch((error) => console.error(error));
