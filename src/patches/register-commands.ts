f94d5ead8547fc62b343efcb3f29fc218346f145
MAOZ <maooz4426@gmail.com>
2026-08-14 14:15:04 +0900

    patchを実装

--- src/patches/register-commands.ts
+++ src/patches/register-commands.ts
@@ -4,16 +4,16 @@ import { commandList } from "../commands";
 
 // 登録用関数
 async function registerCommands() {
-    assert.ok(process.env.TOKEN, "TOKEN is required");
-    assert.ok(process.env.CLIENT_ID, "CLIENT_ID is required");
+	assert.ok(process.env.TOKEN, "TOKEN is required");
+	assert.ok(process.env.CLIENT_ID, "CLIENT_ID is required");
 
-    const rest = new REST().setToken(process.env.TOKEN);
+	const rest = new REST().setToken(process.env.TOKEN);
 
-    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
-        body: commandList.map((command) => command.data.toJSON()),
-    });
+	await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+		body: commandList.map((command) => command.data.toJSON()),
+	});
 
-    console.log("finished");
+	console.log("finished");
 }
 
 registerCommands().catch((error) => console.error(error));
